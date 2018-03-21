481a2176d038c160261d85a39a3a1932c2780e62
GALTdea <gustavovalenzuela1180@gmail.com>
2018-03-21 15:44:53 -0700

    added and set up carrierwave, added business image to bussiness and added image helper

--- app/helpers/businesses_helper.rb
+++ app/helpers/businesses_helper.rb
@@ -1,2 +1,13 @@
 module BusinessesHelper
+  def image_generator(height:, width:)
+    "http://placehold.it/#{height}x#{width}"
+  end
+
+  def portfolio_img f
+    if f.business_image?
+      img
+    else
+      image_generator(height: '350', width: '200')
+    end
+  end
 end
