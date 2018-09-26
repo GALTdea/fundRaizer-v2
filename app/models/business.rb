606cb22765073dac64ef2aaeb85c031eaa477e99
GALTdea <gustavovalenzuela1180@gmail.com>
2018-09-25 18:16:02 -0700

    added friendly id gem to both types of users

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,9 +1,17 @@
 class Business < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+
+
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+  extend FriendlyId
+  friendly_id :name, use: :slugged
 
 
 
@@ -12,5 +20,11 @@ class Business < ApplicationRecord
 
   has_many :events, dependent: :destroy
   has_many :orgs, through: :events
+
+
+  
+
+
+  #alias_attribute :slug, :column_name
 
 end
--- app/models/org.rb
+++ app/models/org.rb
@@ -1,6 +1,10 @@
 class Org < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
